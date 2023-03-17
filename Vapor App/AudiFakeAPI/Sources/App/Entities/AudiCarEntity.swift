f7fa4f2f0fcb307ef18ac37bc0fee81950e720cd
Javier Cruz Santiago (89974) <javier.cruzs@bancoazteca.com>
2023-03-17 14:40:54 -0600

    [NEW] - Added path to get car version details, and added A1 Sportback json file

--- Vapor App/AudiFakeAPI/Sources/App/Entities/AudiCarEntity.swift
+++ Vapor App/AudiFakeAPI/Sources/App/Entities/AudiCarEntity.swift
@@ -31,7 +31,8 @@ struct AudiCarVersion: Content {
     var versionId: String?
     var versionName: String?
     var initialPrice: Double?
-    var imageUrl: String?
+    var coverImageUrl: String?
+    var frontImageUrl: String?
     var stock: Int?
     
     func encode(to encoder: Encoder) throws {
@@ -39,7 +40,8 @@ struct AudiCarVersion: Content {
         try container.encode(self.versionId, forKey: .versionId)
         try container.encode(self.versionName, forKey: .versionName)
         try container.encode(self.initialPrice, forKey: .initialPrice)
-        try container.encode(self.imageUrl, forKey: .imageUrl)
+        try container.encode(self.coverImageUrl, forKey: .coverImageUrl)
+        try container.encode(self.frontImageUrl, forKey: .frontImageUrl)
         try container.encode(self.stock, forKey: .stock)
     }
 }
