d5681ba72260caabe23db09be5b2b0a97e82c409
Javier Cruz Santiago (89974) <javier.cruzs@bancoazteca.com>
2023-03-17 12:16:02 -0600

    [NEW] - Added json Detail for Audi A1 Sportback

--- Vapor App/AudiFakeAPI/Sources/App/routes.swift
+++ Vapor App/AudiFakeAPI/Sources/App/routes.swift
@@ -2,16 +2,8 @@ import Foundation
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
-    
-    app.get("CarsList.json") { req async throws -> Response in
-        let filePath = req.application.directory.publicDirectory + "CarsList.json"
+    app.get("CarList.json") { req async throws -> Response in
+        let filePath = req.application.directory.publicDirectory + "CarList.json"
         guard let fileData = FileManager.default.contents(atPath: filePath) else {
             throw Abort(.notFound)
         }
@@ -21,11 +13,23 @@ func routes(_ app: Application) throws {
     }
     
     app.get("audiAPI", "carList") { req async throws -> [AudiCarEntity] in
-        let filePath = req.application.directory.publicDirectory + "CarsList.json"
+        let filePath = req.application.directory.publicDirectory + "CarList.json"
         guard let fileData = FileManager.default.contents(atPath: filePath) else {
             throw Abort(.notFound)
         }
         let cars: [AudiCarEntity] = try JSONDecoder().decode([AudiCarEntity].self, from: fileData)
         return cars
+    }
+    
+    app.get("audiAPI",":model",":version") { req async throws -> Response in
+        guard let model = req.parameters.get("model"),
+              let version = req.parameters.get("version") else {
+            throw Abort(.internalServerError)
+        }
+        let filePath = "\(req.application.directory.publicDirectory)/\(model)/\(version).json"
+        guard let fileData = FileManager.default.contents(atPath: filePath) else {
+            throw Abort(.notFound)
+        }
+        return Response(status: .ok, body: .init(data: fileData))
     }
 }
