934622e4725ac305223becf777ba191612a4c83b
Javier Cruz Santiago (89974) <javier.cruzs@bancoazteca.com>
2023-03-28 22:38:48 -0600

    [UPDATE] - Updated Server config in order to be visible for your local network

--- Vapor App/AudiFakeAPI/Sources/App/configure.swift
+++ Vapor App/AudiFakeAPI/Sources/App/configure.swift
@@ -2,6 +2,9 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
+    // coment if you don't want to expose your server in your local network
+    app.http.server.configuration.address = BindAddress.hostname("0.0.0.0", port: 8080)
+    
     // uncomment to serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
